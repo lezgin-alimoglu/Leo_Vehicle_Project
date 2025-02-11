9cd653b7145c9bffa7e1bff35a4a47589341a619
Lezgin Alimoğlu <lezgin2003@gmail.com>
2025-02-11 13:42:45 +0300

    Update motorControl.ino

--- src/motorControl.ino
+++ src/motorControl.ino
@@ -1,82 +1,91 @@
 #include <mcp2515_can.h>
 #include <SPI.h>
 
-// SAMD çekirdek uyumluluğu
+// SAMD Core
 #ifdef ARDUINO_SAMD_VARIANT_COMPLIANCE
 #define SERIAL SerialUSB
 #else
 #define SERIAL Serial
 #endif
 
-// CS pini
+// CS Pin
 mcp2515_can CAN(10);
 
-// Sabitler
-const float MAX_RPM = 100000.0f;  // Maksimum RPM sınırı
-float rpm = 0.0;                 // Başlangıç RPM değeri
+// Maksimum değerler
+const float MAX_RPM = 10000.0f;
 
-// CAN mesaj kimlik hesaplama
-uint32_t canId(int id, int mode) {
-  return uint32_t(id | mode << 8);
-}
+// Motor kimlikleri
+const uint8_t MOTOR_1 = 101;
+const uint8_t MOTOR_2 = 102;
+const uint8_t MOTOR_3 = 103;
+const uint8_t MOTOR_4 = 104;
 
-// CAN veri gönderme fonksiyonu
-void comm_can_transmit_eid(uint32_t id, const uint8_t* data, uint8_t len) {
-  if (len > 8) len = 8;  // CAN verisi maksimum 8 bayt olabilir
-  CAN.sendMsgBuf(id, 1, len, data);
-}
-
-// 32-bit integeri buffer'a ekle
-void buffer_append_int32(uint8_t* buffer, int32_t number, int32_t* index) {
-  buffer[(*index)++] = number >> 24;
-  buffer[(*index)++] = number >> 16;
-  buffer[(*index)++] = number >> 8;
-  buffer[(*index)++] = number;
-}
-
-// RPM ayarı için CAN mesajı gönder
-void comm_can_set_rpm(uint8_t controller_id, float rpm_value) {
+// Fonksiyonlar
+void comm_can_set_rpm(uint8_t controller_id, float rpm) {
   int32_t send_index = 0;
   uint8_t buffer[4];
-  buffer_append_int32(buffer, (int32_t)rpm_value, &send_index);
-  comm_can_transmit_eid(canId(controller_id, 3), buffer, send_index); // 3, Velocity modunu temsil ediyor
+  buffer[send_index++] = (int32_t)rpm >> 24;
+  buffer[send_index++] = (int32_t)rpm >> 16;
+  buffer[send_index++] = (int32_t)rpm >> 8;
+  buffer[send_index++] = (int32_t)rpm;
+  CAN.sendMsgBuf(canId(controller_id, AKMode::AK_VELOCITY), 1, send_index, buffer);
+}
+
+// Komutları işleme
+void processCommand(String command) {
+  float rpm = MAX_RPM; // Tüm motorlar için varsayılan RPM
+
+  if (command == "move:up") {
+    // Yukarı hareket - Tüm motorlar aynı yönde
+    comm_can_set_rpm(MOTOR_1, rpm);
+    comm_can_set_rpm(MOTOR_2, rpm);
+    comm_can_set_rpm(MOTOR_3, rpm);
+    comm_can_set_rpm(MOTOR_4, rpm);
+  } else if (command == "move:down") {
+    // Aşağı hareket - Ters yönde
+    comm_can_set_rpm(MOTOR_1, -rpm);
+    comm_can_set_rpm(MOTOR_2, -rpm);
+    comm_can_set_rpm(MOTOR_3, -rpm);
+    comm_can_set_rpm(MOTOR_4, -rpm);
+  } else if (command == "move:left") {
+    // Sola hareket - Sağ motorlar ileri, sol motorlar geri
+    comm_can_set_rpm(MOTOR_1, -rpm);
+    comm_can_set_rpm(MOTOR_2, rpm);
+    comm_can_set_rpm(MOTOR_3, -rpm);
+    comm_can_set_rpm(MOTOR_4, rpm);
+  } else if (command == "move:right") {
+    // Sağa hareket - Sol motorlar ileri, sağ motorlar geri
+    comm_can_set_rpm(MOTOR_1, rpm);
+    comm_can_set_rpm(MOTOR_2, -rpm);
+    comm_can_set_rpm(MOTOR_3, rpm);
+    comm_can_set_rpm(MOTOR_4, -rpm);
+  } else {
+    // Hatalı komut - Tüm motorları durdur
+    comm_can_set_rpm(MOTOR_1, 0);
+    comm_can_set_rpm(MOTOR_2, 0);
+    comm_can_set_rpm(MOTOR_3, 0);
+    comm_can_set_rpm(MOTOR_4, 0);
+  }
 }
 
 void setup() {
-  // Seri haberleşme başlat
-  Serial.begin(115200);
-  while (!Serial) {}
+  SERIAL.begin(115200);
+  while (!SERIAL) {};
 
-  // CAN modülünü başlat
-  while (CAN_OK != CAN.begin(CAN_1000KBPS)) {
-    Serial.println("CAN BUS modülü başlatılamadı!");
-    delay(100);
+  if (CAN.begin(CAN_1000KBPS) != CAN_OK) {
+    SERIAL.println("CAN BUS Shield init fail");
+    while (1);
   }
-  Serial.println("CAN modülü başlatıldı.");
+
+  SERIAL.println("CAN init ok!");
 }
 
 void loop() {
-  // Raspberry Pi'den veri oku
-  if (Serial.available()) {
-    char received = Serial.read();
-
-    // Gamepad girişine göre RPM ayarla
-    if (received == 'x') {
-      rpm += 2500.0;
-      if (rpm > MAX_RPM) rpm = MAX_RPM; // RPM'i maksimum sınırda tut
-      Serial.println("RPM artırıldı.");
-    } else if (received == 's') {
-      rpm -= 2500.0;
-      if (rpm < 0) rpm = 0; // RPM'i sıfırdan düşük tutma
-      Serial.println("RPM azaltıldı.");
-    }
+  // Seri porttan komut oku
+  if (SERIAL.available()) {
+    String command = SERIAL.readStringUntil(';'); // Komut ';' ile sonlanır
+    processCommand(command);
   }
 
-  // CAN mesajlarını gönder
-  comm_can_set_rpm(104, rpm); // Motor ID'si 104
-  comm_can_set_rpm(103, rpm); // Motor ID'si 103
-  comm_can_set_rpm(102, rpm); // Motor ID'si 102
-  comm_can_set_rpm(101, rpm); // Motor ID'si 101
-
-  delay(10); // Motor kontrolünü stabil tutmak için kısa gecikme
+  delay(10); // Ana döngü gecikmesi
 }
